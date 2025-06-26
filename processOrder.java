7543f64e60f4a65342a9d44f04844ac95190190b
Djonathan D'Avila <123762143+DjonathanDAvila@users.noreply.github.com>
2025-06-25 21:30:13 -0300

    Limpando repositorio

--- processOrder.java
+++
@@ -1,7 +0,0 @@
-public void processOrder(OrderDto dto) {
-    if (!dto.isValid()) throw new IllegalArgumentException();
-    Order order = new Order(dto);
-    orderRepository.save(order);
-    emailService.sendConfirmation(order.getCustomerEmail());
-    logger.info("Order processed: " + order.getId());
-}
